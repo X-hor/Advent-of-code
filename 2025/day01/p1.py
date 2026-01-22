678ebc65d6b497b2e05d3e63d4a12c0044446cf5
X-hor <187279977+X-hor@users.noreply.github.com>
2026-01-22 02:56:29 +0100

    Fix .gitignore - remove tracked files

--- 2025/day01/p1.py
+++
@@ -1,6 +0,0 @@
-p, count = 50, 0
-for line in open("input.txt"):
-    direction, distance = line[0], int(line[1:])
-    p = (p + (distance if direction == "R" else -distance)) % 100
-    count += (p == 0)
-print(count)
--- 2025/day06/p1.py
+++
@@ -1,8 +0,0 @@
-lines = [line.strip().split() for line in open("input.txt")]
-cols = list(zip(*lines))
-total = 0
-for *numbers, op in cols:
-    total += eval(op.join(numbers))
-print(total)
-#  One-liner potential
-print(sum(eval(op.join(nums)) for *nums, op in zip(*[line.split() for line in open("input.txt")])))
